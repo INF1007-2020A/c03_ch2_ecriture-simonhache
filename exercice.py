dc29886706f876a6acff5e52270aa340a645113e
simonhache <simonhache17@gmail.com>
2020-09-09 14:42:26 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,6 +5,7 @@ def majuscule(mot):
     for lettre in mot:
         Dec_Lettre=ord(lettre)
         if Dec_Lettre <=122 & Dec_Lettre>=97
+            New_Letre
         # TODO completer la fonction ici
         
         resultat += lettre
