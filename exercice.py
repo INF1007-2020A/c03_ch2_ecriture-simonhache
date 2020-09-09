f0a17aa0eff055824389fb30bfeaa43dd8e947f5
simonhache <simonhache17@gmail.com>
2020-09-09 15:19:26 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -4,7 +4,7 @@ def majuscule(mot):
     resultat = ''
     for lettre in mot:
         lettre=ord(lettre)
-            lettre-=32
+        lettre-=32
         resultat += chr(lettre)
     return mot
 
