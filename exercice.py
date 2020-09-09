28b6e8b44de7440cdf9aa7f8fcf0f49b622846aa
simonhache <simonhache17@gmail.com>
2020-09-09 14:41:05 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -3,7 +3,7 @@
 def majuscule(mot):
     resultat = ''
     for lettre in mot:
-        ord(lettre)
+        DecLettreord(lettre)
         # TODO completer la fonction ici
         
         resultat += lettre
